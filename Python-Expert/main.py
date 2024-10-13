ba7177bc9eb6ff0bb728a8710f4be82df8d57a30
Arturo0911 <anegreiross@outlook.com>
2024-10-13 00:25:51 -0500

    ok

---
+++ Python-Expert/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+class Polynomial:
+    def __init__(self, *coeffs) -> None:
+        self.coeffs = coeffs
+        
+    def __repr__(self) -> str:
+        return "Polynomial(*{!r})".format(self.coeffs)
+
+    def __add__(self, other):
+        return Polynomial(*(x + y for x, y in zip(self.coeffs, other.coeffs)))
+
+# def main():
+p1 = Polynomial(2, 3, 4) # 2x² + 3x + 4
+p2 = Polynomial(3, 3, 3) # 3x² + 3x + 3    
